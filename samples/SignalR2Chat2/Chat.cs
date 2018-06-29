05caa597f0437c4f6f8de8f5a1b3fef9880d66d8
Liangying Wei <lianwei@microsoft.com>
2018-06-29 10:19:13 +0800

    Group related works

--- samples/SignalR2Chat2/Chat.cs
+++ samples/SignalR2Chat2/Chat.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace SignalR2Chat2
 {
--- src/Microsoft.Azure.AspNet.SignalR/ServiceMessageBus.cs
+++ src/Microsoft.Azure.AspNet.SignalR/ServiceMessageBus.cs
@@ -2,14 +2,17 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Infrastructure;
 using Microsoft.AspNet.SignalR.Json;
 using Microsoft.AspNet.SignalR.Messaging;
 using Microsoft.AspNet.SignalR.Transports;
 using Microsoft.Azure.SignalR;
 using Microsoft.Azure.SignalR.Protocol;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.AspNet.SignalR
@@ -26,11 +29,17 @@ namespace Microsoft.Azure.AspNet.SignalR
 
         private const string ConnectionIdPrefix = "c-";
 
+        private readonly JsonSerializer _serializer;
+
         private readonly IServiceConnectionManager _serviceConnectionManager;
+
+        private readonly IAckHandler _ackHandler;
 
         public ServiceMessageBus(IDependencyResolver resolver) : base(resolver)
         {
             _serviceConnectionManager = resolver.Resolve<IServiceConnectionManager>();
+            _serializer = resolver.Resolve<JsonSerializer>();
+            _ackHandler = resolver.Resolve<IAckHandler>();
         }
 
         public override async Task Publish(Message message)
@@ -60,27 +69,89 @@ namespace Microsoft.Azure.AspNet.SignalR
                 ms.TryGetBuffer(out segment);
             }
 
-            // Which hub?
-            if (message.Key.StartsWith(HubPrefix))
-            {
-                var hubName = message.Key.Substring(HubPrefix.Length);
-                await _serviceConnectionManager.WithHub(hubName).WriteAsync(new BroadcastDataMessage(excludedList: null, payloads: GetPayload(segment)));
-            }
-            // Which group?
-            else if (message.Key.StartsWith(HubGroupPrefix))
-            {
-                await _serviceConnectionManager.WriteAsync(new GroupBroadcastDataMessage(message.Key.Substring(HubGroupPrefix.Length), excludedList: null, payloads: GetPayload(segment)));
-            }
-            else if (message.Key.StartsWith(ConnectionIdPrefix))
-            {
-                await _serviceConnectionManager.WriteAsync(new ConnectionDataMessage(message.Key.Substring(ConnectionIdPrefix.Length), segment));
-            }
-
             if (message.IsCommand)
             {
-                // TODO: handle commands
-                await base.Publish(message);
+                var command = _serializer.Parse<Command>(message.Value, message.Encoding);
+                switch (command.CommandType)
+                {
+                    case CommandType.AddToGroup:
+                        {
+                            // name is hg-{HubName}.{GroupName}, consider the whole as the actual group name
+                            var name = command.Value;
+
+                            if (message.Key.StartsWith(ConnectionIdPrefix))
+                            {
+                                var connectionId = message.Key.Substring(ConnectionIdPrefix.Length);
+
+                                // Hub name can have '.', and group name can have '.', is there a way to determine which hub this group is in?
+                                await _serviceConnectionManager.WriteAsync(new JoinGroupMessage(connectionId, name));
+                                _ackHandler.TriggerAck(message.CommandId);
+                            }
+                        }
+                        break;
+                    case CommandType.RemoveFromGroup:
+                        {
+                            var name = command.Value;
+
+                            if (message.Key.StartsWith(ConnectionIdPrefix))
+                            {
+                                var connectionId = message.Key.Substring(ConnectionIdPrefix.Length);
+
+                                await _serviceConnectionManager.WriteAsync(new LeaveGroupMessage(connectionId, name));
+                            }
+                        }
+                        break;
+                    case CommandType.Initializing:
+                        break;
+                    case CommandType.Abort:
+                        break;
+                }
             }
+            else
+            {
+                // broadcast case
+                if (message.Key.StartsWith(HubPrefix))
+                {
+                    var hubName = message.Key.Substring(HubPrefix.Length);
+                    await _serviceConnectionManager.WithHub(hubName).WriteAsync(new BroadcastDataMessage(excludedList: GetExcludedIds(message.Filter), payloads: GetPayload(segment)));
+                }
+                // echo case
+                if (message.Key.StartsWith(HubConnectionIdPrefix))
+                {
+                    var hubAndConnectionId = message.Key.Substring(HubConnectionIdPrefix.Length);
+
+                    // ConnectionID is base64 encoded, it does not contain '.'
+                    var index = hubAndConnectionId.LastIndexOf('.');
+                    var hub = hubAndConnectionId.Substring(0, index);
+                    var connectionId = hubAndConnectionId.Substring(index + 1);
+                    await _serviceConnectionManager.WithHub(hub).WriteAsync(new ConnectionDataMessage(connectionId, segment));
+                }
+                else if (message.Key.StartsWith(HubGroupPrefix))
+                {
+                    await _serviceConnectionManager.WriteAsync(new GroupBroadcastDataMessage(message.Key, excludedList: GetExcludedIds(message.Filter), payloads: GetPayload(segment)));
+                }
+                else if (message.Key.StartsWith(ConnectionIdPrefix))
+                {
+                    await _serviceConnectionManager.WriteAsync(new ConnectionDataMessage(message.Key.Substring(ConnectionIdPrefix.Length), segment));
+                }
+            }
+        }
+
+        private IReadOnlyList<string> GetExcludedIds(string filter)
+        {
+            return filter?.Split('|').Select(GetConnectionId).Where(s => s != null).ToArray();
+        }
+
+        private string GetConnectionId(string prefixedId)
+        {
+            if (prefixedId.StartsWith(ConnectionIdPrefix))
+            {
+                return prefixedId.Substring(ConnectionIdPrefix.Length);
+            }
+
+            // TODO: what to do for invalid data?
+            // throw new InvalidDataException($"connection id should start with {ConnectionIdPrefix} however it is {prefixedId}");
+            return null;
         }
     }
 }
